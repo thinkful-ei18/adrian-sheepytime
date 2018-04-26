bb0cb321b1467401b058b74cd64c86bab2227495
Adrian <adriantoddross@gmail.com>
2018-04-26 15:23:26 -0400

    add basic styling

--- components/home.js
+++ components/home.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import {Text, View } from 'react-native';
+import {Alert, StyleSheet, View } from 'react-native';
+import { Text, Button } from 'react-native-elements';
 
 export default function home(props) {
   return (
-    <View>
-      <Text>Sheepytime</Text>
+    <View style={styles.container}>
+      <Text>I'm going to bed now, when should I wake up?</Text>
+      <Button
+        large
+        title='Going to bed now'
+       onPress={() => {Alert.alert('You tapped the button!');}}/>
+      <Text>I need to pick a certain time, when should I wake up or go to bed?</Text>
+      <Button
+        large
+        title='Pick a time'/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
